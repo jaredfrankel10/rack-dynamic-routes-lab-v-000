deef0a130af6d93ab7b59b3e738351a5c712a0b2
jared frankel <jaredfrankel10@gmail.com>
2018-01-30 19:15:27 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,15 +1,15 @@
 class Application
  @@items = []
- 
+
    def call(env)
      resp = Rack::Response.new
      req = Rack::Request.new(env)
- 
+
      if req.path.match(/items/)
- 
+
        item_name =
        req.path.split("/items/").last
- 
+
        if item = @@items.find {|i| i.name == item_name}
          resp.write item.price
        else
@@ -22,5 +22,5 @@ class Application
      end
      resp.finish
    end
- 
+
  end
